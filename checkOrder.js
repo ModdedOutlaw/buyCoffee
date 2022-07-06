aa123453d90f123e54ef0df6524379d5ad21a4d9
ModdedOutlaw <moddedoutlaw33@gmail.com>
2022-07-06 13:45:12 -0400

    x

--- checkOrder.js
+++ checkOrder.js
@@ -2,4 +2,3 @@ const cust_name = sessionStorage.getItem('name');
 const transferNum = sessionStorage.getItem('transferId');
 console.log('CUSTOMER-NAME: ' +cust_name);
 document.getElementById('customer-name').innerHTML += '<b>'+cust_name+'</b><br><b>WAX transaction Number: '+transferNum+'</b>';
-sessionStorage.clear();
